a729a6f00d3fba25856d350b0c847c2fe40eb926
jasunde <jlaurits@gmail.com>
2016-11-02 22:15:29 -0500

    Added price change functions

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ $(document).ready(function () {
 		{type: 'banana', price: 5},
 		{type: 'orange', price: 5},
 		{type: 'grape', price: 5},
-	];
+	],
+	$market = $('#market');
 
+	// Add items to DOM
 	fruits.forEach(function (fruit) {
 		var $item = $('<div></div>').addClass('item');
 		var $fruit = $('<div></div>').addClass('fruit ' + fruit.type);
@@ -13,7 +15,7 @@ $(document).ready(function () {
 		$item.append('<button>Buy</button>');
 		$item.append('<p>Price: ' + fruit.price + '</p>');
 
-		$('#market').append($item);
+		$market.append($item);
 
 		var $ul = $('<ul></ul>'),
 				$number = $('<li></li>').addClass(fruit.type).text(fruit.type + 's: '),
@@ -23,6 +25,18 @@ $(document).ready(function () {
 		$('#inventory').append($ul);
 	});
 
+	cyclePrices();
+
+	function changePrices() {
+		fruits.forEach(function (fruit) {
+			fruit.price += randomNumber(-50, 50) / 100;
+			$market.find('.' + fruit.type).siblings().last().text('Price: ' + fruit.price.toLocaleString('en-US', {style: 'currency', currency: 'USD'}));
+		});
+	}
+
+	function cyclePrices() {
+		setInterval(changePrices, 5000);
+	}
 
 });
 function randomNumber(min, max) {
